c877ce6e855761b49052a2dbfcce10e444d60ea4
JimmyKodu <47095774+JimmyKodu@users.noreply.github.com>
2024-02-05 10:29:27 +0800

    添加项目文件。

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var result = GetWeather();
+            if (result.Result == null)
+            {
+                await DisplayAlert("警报", result.Reason, "确定");
+                return;
+            }
+            BindingContext = result.Result.RealTime;
+            listBox_Future.ItemsSource = result.Result.Future;
+            image_Info.Source = $"id_{result.Result.RealTime.Wid}.gif";
+        }
+        public WeatherResult GetWeather()
+        {
+            var city = this.txt_City.Text.Trim();
+            string key = "44c38f50a4d7c7e9601abc0cd7f50be0";
+            string url = @"http://apis.juhe.cn/simpleWeather/query?city=" + city + "&key=" + key;
+            HttpWebRequest webRequest = HttpWebRequest.Create(url) as HttpWebRequest; ;
+            webRequest.Method = "GET";
+            webRequest.ContentType = "text/html";
+            using (StreamReader sr = new StreamReader(webRequest.GetResponse().GetResponseStream()))
+            {
+                string str = sr.ReadToEnd();
+                var result = JsonConvert.DeserializeObject<WeatherResult>(str);
+                return result;
+            }
+        }
+    }
+
+}
---
+++ WeatherApp/WeatherResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherResult
+    {
+        public string Reason { get; set; }
+        public WeatherDetail Result { get; set; }
+        public string Error_Code { get; set; }
+    }
+    public class WeatherDetail
+    {
+        public string City { get; set; }
+        public WeatherRealTime RealTime { get; set; }
+        public List<WeatherFuture> Future { get; set; }
+    }
+
+    public class WeatherRealTime
+    {
+        public string Temperature { get; set; }
+        public string Humidity { get; set; }
+        public string Info { get; set; }
+        public string Wid { get; set; }
+        public string Direct { get; set; }
+        public string Power { get; set; }
+        public string Aqi { get; set; }
+    }
+
+    public class WeatherFuture
+    {
+        public string Date { get; set; }
+        public string Temperature { get; set; }
+        public string Weather { get; set; }
+        public Wid Wid { get; set; }
+        public string Direct { get; set; }
+    }
+
+    public class Wid
+    {
+        public string Day { get; set; }
+        public string Night { get; set; }
+    }
+}
